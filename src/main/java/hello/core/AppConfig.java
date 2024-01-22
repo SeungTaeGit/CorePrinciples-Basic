d7b823dbd1de2abd8b9c96f0cf85ec50e0268865
SeungTae <dltmdxo1589@daum.net>
2024-01-23 01:28:12 +0900

    commit

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    // 생성자 주입
+    public MemberService memberService() {
+        return new MemberServiceImpl(MemberRepository());
+    }
+
+    private MemoryMemberRepository MemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
